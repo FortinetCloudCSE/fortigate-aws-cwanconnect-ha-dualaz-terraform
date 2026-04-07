845eaef4690261ca15133bb9094d128e2a56a443
hgaberra <hgaberra@gmail.com>
2026-04-07 17:00:52 -0400

    remove access/secret key vars and tf fmt

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-  type    = string
-  default = ""
-}
-variable "secret_key" {
-  type    = string
-  default = ""
-}
 variable "region" {
   description = "Provide the region to deploy the VPC in"
   type        = string
